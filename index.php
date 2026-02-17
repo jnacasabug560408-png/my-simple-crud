19dac487ecc751c4b7279d96e6d495daa5fec937
JillianGraceNacasabug <j.nacasabug.560408@umindanao.edu.ph>
2026-02-17 17:55:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ include 'components/pdo.php';
 $sql = "UPDATE users SET firstname = ?, lastname = ? WHERE id = ?";
 $stmt = $pdo->prepare($sql);
 
-$newFirstname = "Dave";
-$newLastname  = "Cabeza";
+$newFirstname = "Jillian";
+$newLastname  = "Nacasabug";
 $id = 6; // change this to the id you want to update
 
 $stmt->execute([$newFirstname, $newLastname, $id]);
 
 echo "Updated rows: " . $stmt->rowCount();
+
 ?>
